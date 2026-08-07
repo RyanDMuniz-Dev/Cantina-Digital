a8ea48d42791bbe6ac7bb8466965bd0a854825e9
Ryan <ryandmuniz@gmail.com>
2026-08-07 05:59:23 -0300

    feat: inicio da reformulação de pedido e atualização de produto.

--- app/src/main/java/com/example/cantinadigital/data/model/OrderItem.kt
+++ app/src/main/java/com/example/cantinadigital/data/model/OrderItem.kt
@@ -13,4 +13,10 @@ data class OrderItem(
     @get:PropertyName("valor_unidade") @set:PropertyName("valor_unidade")
     var unitValue: Double = 0.0,
 
+    @get:PropertyName("vendedor") @set:PropertyName("vendedor")
+    var vendedor: String = "Cantina",
+
+    @get:PropertyName("taxa_cantina") @set:PropertyName("taxa_cantina")
+    var taxaCantinaPorcentagem: Double = 0.0
+
 )
